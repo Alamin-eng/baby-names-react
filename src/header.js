8d9de3a9af64d195725886d9240b4938589682ee
Alamin-eng <alaminruben@gmail.com>
2022-03-25 23:23:17 +0000

    first commit

---
+++ src/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header className="header-div">Baby Name Picker</header>
+    </div>
+  );
+}
+
+export default Header;
