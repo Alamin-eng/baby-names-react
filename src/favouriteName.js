95d669ac05d61fecb08f6e716db3837c2db82797
Alamin-eng <alaminruben@gmail.com>
2022-04-22 00:44:14 +0100

    build installed

--- src/favouriteName.js
+++ src/favouriteName.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import BabyName from "./babyName";
 
 const Favourites = ({ babyNames, favouritesNames, setFavouritesNames }) => {
